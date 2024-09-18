505dd17a558d1db8eb707b99679834f288dff0cc
Paulius <chingfdjflskjfsd@fjsfkl.sdf>
2024-09-18 21:05:37 +0300

    Add parsing utilities

---
+++ src/Parsing.cpp
@@ -0,0 +1,28 @@
+#include "Parsing.h"
+#include "FlatMap.h"
+
+using namespace Parsing;
+
+#pragma region Lookup Tables
+
+FlatMap<ParsingResult, std::string> const exceptionStr({
+    {ParsingResult::Ok, "Ok"},
+    {ParsingResult::MultipleColons, "Multiple colons in a single line"},
+    {ParsingResult::IncorrectLabel,
+     "Incorrect label (must be an integer in the range 0-255)"},
+});
+
+#pragma endregion
+
+char const* ParsingException::what() const noexcept {
+    auto const resultStr = exceptionStr.Find(result);
+    std::string const explain =
+        resultStr.has_value() ? *resultStr : "Unknown error";
+
+    return ("Exception occurred during parsing: " + explain).c_str();
+}
+
+std::pair<uint16_t, Command>
+Parsing::ParseCommandString(std::string const& str) {
+    throw std::logic_error("Not implemented");
+}
---
+++ src/Parsing.h
@@ -0,0 +1,67 @@
+#ifndef __PARSING_H
+#define __PARSING_H
+
+#include <cstdint>
+#include <exception>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace Parsing {
+enum class Operation {
+    Read,
+    Next,
+    Mov,
+    Reset,
+    Ls,
+    Add,
+    Not,
+    Xor,
+    MuxIn,
+    Inc,
+    Dec,
+    Ll1,
+    Lr1,
+    Al1,
+    Ar1,
+    Cl1,
+    Cr1,
+};
+enum class Operand {
+    RegisterA,
+    RegisterB,
+    RegisterC,
+    RegisterD,
+    RegisterE,
+    RegisterF,
+    Counter,
+    Rom,
+    Flag,
+};
+
+struct CommandEntry {
+    Operation operation;
+    std::vector<std::variant<Operand, uint16_t>> operands;
+};
+
+using Command = std::vector<CommandEntry>;
+
+enum class ParsingResult {
+    Ok,
+    MultipleColons,
+    IncorrectLabel,
+};
+
+class ParsingException : public std::exception {
+    ParsingResult const result;
+
+  public:
+    ParsingException(ParsingResult result) : result{result} {}
+    char const* what() const noexcept override;
+};
+
+std::pair<uint16_t, Command> ParseCommandString(std::string const& str);
+
+} // namespace Parsing
+
+#endif
